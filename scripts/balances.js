3fa5b5373f6ada11e00e6af5736659509cbf9286
Samir Antonio <samir.ma@gmail.com>
2022-10-15 20:41:39 +0100

    fix balence

--- scripts/balances.js
+++ scripts/balances.js
@@ -1,26 +1,28 @@
 const hre = require("hardhat");
 const fs = require("fs");
 require("dotenv").config();
+const lib = require("./trade_lib");
+
+require("dotenv").config({ path: "../.env" });
+const wallet_address = process.env.address;
 
 let config,arb,owner;
 const network = hre.network.name;
-if (network === 'aurora') config = require('./../config/aurora.json');
-if (network === 'fantom') config = require('./../config/fantom.json');
+
+config = lib.config;
 
 const main = async () => {
 	[owner] = await ethers.getSigners();
-  console.log(`Owner: ${owner.address}`);
-  const IArb = await ethers.getContractFactory('Arb');
-  arb = await IArb.attach(config.arbContract);
-	const interface = await ethers.getContractFactory('WETH9');
-  for (let i = 0; i < config.baseAssets.length; i++) {
-    const asset = config.baseAssets[i];
-		const tokenAsset = await interface.attach(asset.address);
+	console.log(`Owner: ${owner.address}`);
+	arb = await lib.getArbContract();
+	for (let i = 0; i < config.baseAssets.length; i++) {
+		const asset = config.baseAssets[i];
+		const tokenAsset = await lib.getToken(asset.address);
 		const ownerBalance = await tokenAsset.balanceOf(owner.address);
-    console.log(`${asset.sym} Owner Balance: `,ownerBalance.toString());
+		console.log(`${asset.sym} Owner Balance: `,ownerBalance.toString());
 		const arbBalance = await arb.getBalance(asset.address);
 		console.log(`${asset.sym} Arb Balance: `,arbBalance.toString());
-  }
+	}
 }
 
 process.on('uncaughtException', function(err) {
